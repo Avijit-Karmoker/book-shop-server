8507fcb585e689df28e1ed088494a1a2065ca996
Avijit Karmoker <ovijit1hsc@gmail.com>
2021-04-07 14:33:16 +0600

    port updated and nodemon fix

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  const newEvent = req.body;
-    booksCollection.insertOne(newEvent).then((result) => {
-      res.send(result.insertedCount > 0);
-    });
+  res.send("Hello World!");
 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2zom1.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
